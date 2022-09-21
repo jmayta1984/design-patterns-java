fd59f6e34ead90eaa729c36f97eb8a77b820b846
Jorge Mayta <jmayta@pucp.edu.pe>
2022-09-21 09:31:57 -0500

    chain of responsibility pattern

---
+++ src/main/java/behavioral/chain/DispenseChain.java
@@ -0,0 +1,8 @@
+package behavioral.chain;
+
+public interface DispenseChain {
+
+    void setNextChain(DispenseChain nextChain);
+
+    void dispense(Currency currency);
+}
---
+++ src/main/java/structural/proxy/Demo.java
@@ -0,0 +1,15 @@
+package structural.proxy;
+
+public class Demo {
+
+    public static void main(String[]args){
+        Image image = new ProxyImage("pikachu.png");
+
+        image.display();
+
+        System.out.println("");
+
+        image.display();
+
+    }
+}
