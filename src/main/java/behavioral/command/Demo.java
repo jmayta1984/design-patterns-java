e0a098efc0c2c387506fdf6772414505b8243b97
Jorge Mayta <jmayta@pucp.edu.pe>
2022-09-24 08:03:12 -0500

    command pattern

---
+++ src/main/java/behavioral/command/Demo.java
@@ -0,0 +1,19 @@
+package behavioral.command;
+
+public class Demo {
+
+    public static void main(String[] args){
+
+        Stock stock = new Stock();
+
+        BuyStock buyStockOrder = new BuyStock(stock);
+        SellStock sellStockOrder = new SellStock(stock);
+
+        Broker broker = new Broker();
+
+        broker.takeOrder(buyStockOrder);
+        broker.takeOrder(sellStockOrder);
+
+        broker.placeOrders();
+    }
+}
